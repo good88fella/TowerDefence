08e6e39d0eb4bde09685d01f9558880a5d0e7d5f
Писарев Филипп Михайлович <FMPisarev@sberbank.ru>
2020-03-11 15:25:25 +0300

    g

--- src/main/java/entities/GameMap.java
+++ src/main/java/entities/GameMap.java
@@ -1,50 +1,72 @@
 package entities;
 
+import utils.Rect;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameMap {
 
     private int width;
     private int height;
     private char[][] matrix;
-    private Point start;
-    private Point finish;
+    private Rect.Point start;
+    private Rect.Point finish;
+    private List<Rect> fieldsCoordinates;
 
     public GameMap(int width, int height) {
         this.width = width;
         this.height = height;
         matrix = new char[height][width];
+        fieldsCoordinates = new ArrayList<>();
     }
 
-    public static class Point {
-        private double x;
-        private double y;
-
-        public Point(double x, double y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public double getX() {
-            return x;
-        }
-
-        public double getY() {
-            return y;
+    public void draw() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
         }
     }
 
-    public Point getStart() {
+    public void fillMap() {
+        fieldsCoordinates.add(new Rect(new Rect.Point(0, 0), new Rect.Point(49, 0)));
+        fieldsCoordinates.add(new Rect(new Rect.Point(0, 24), new Rect.Point(49, 24)));
+        fieldsCoordinates.add(new Rect(new Rect.Point(0, 2), new Rect.Point(0, 23)));
+        fieldsCoordinates.add(new Rect(new Rect.Point(49, 2), new Rect.Point(49, 23)));
+
+        fieldsCoordinates.add(new Rect(new Rect.Point(2, 1), new Rect.Point(9, 22)));
+        fieldsCoordinates.add(new Rect(new Rect.Point(11, 2), new Rect.Point(19, 23)));
+        fieldsCoordinates.add(new Rect(new Rect.Point(21, 1), new Rect.Point(27, 22)));
+        fieldsCoordinates.add(new Rect(new Rect.Point(29, 2), new Rect.Point(37, 23)));
+        fieldsCoordinates.add(new Rect(new Rect.Point(39, 1), new Rect.Point(47, 22)));
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++)
+                matrix[i][j] = '#';
+        }
+        for (Rect rect : fieldsCoordinates) {
+            for (int i = (int) rect.getStart().getY(); i <= rect.getEnd().getY(); i++) {
+                for (int j = (int) rect.getStart().getX(); j <= rect.getEnd().getX(); j++) {
+                    matrix[i][j] = '.';
+                }
+            }
+        }
+    }
+
+    public Rect.Point getStart() {
         return start;
     }
 
-    public void setStart(Point start) {
+    public void setStart(Rect.Point start) {
         this.start = start;
     }
 
-    public Point getFinish() {
+    public Rect.Point getFinish() {
         return finish;
     }
 
-    public void setFinish(Point finish) {
+    public void setFinish(Rect.Point finish) {
         this.finish = finish;
     }
 
@@ -70,5 +92,13 @@ public class GameMap {
 
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
+    }
+
+    public List<Rect> getFieldsCoordinates() {
+        return fieldsCoordinates;
+    }
+
+    public void setFieldsCoordinates(List<Rect> fieldsCoordinates) {
+        this.fieldsCoordinates = fieldsCoordinates;
     }
 }
--- src/main/java/entities/Tower.java
+++ src/main/java/entities/Tower.java
@@ -1,8 +1,20 @@
 package entities;
 
+import utils.GameObject;
+
 public class Tower extends GameObject {
 
     public Tower(double x, double y) {
-        super(x, y);
+        super(x, y, 1);
     }
+
+    @Override
+    public void fire() {
+
+    }
+
+    public void upgrade() {
+
+    }
+
 }
